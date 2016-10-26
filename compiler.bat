c7a7b43ed6d249406255cdb07ec560d02d660c2d
Katrina Rondina <rondink@ph.ibm.com>
2016-10-27 04:50:06 +0800

    updated compiler with correct variable names

--- compiler.bat
+++ compiler.bat
@@ -1,24 +1,24 @@
 @echo off
 
 rem -- enter your username and your password
-echo user %katrondina-uname%> ftpcmd.dat
-echo %katpw%>> ftpcmd.dat
+echo user %katrondinausername%> ftpcmd.tmp
+echo %katrondinapassword%>> ftpcmd.tmp
 
 rem -- put "input file" 'PDS/DS destination'
 rem echo put "%WORKSPACE%\DEVOPS.PPLI\HELLOWW.pli" 'b136901.devops.ppli(helloww)'>> ftpcmd.dat
 rem echo put "%WORKSPACE%\DEVOPS.JCL\PLAHELLO.jcl" 'b136901.devops.jcl(plahello)'>> ftpcmd.dat
 
 rem -- to connect to MVS JES Subcommand
-echo quote site filetype=jes>> ftpcmd.dat
+echo quote site filetype=jes>> ftpcmd.tmp
 rem -- ftp compile jcl to jes
 rem echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.dat
 rem -- to view jobs in spool
-echo dir>> ftpcmd.dat
+echo dir>> ftpcmd.tmp
 rem -- to go back to working directory
-echo quote site filetype=seq>> ftpcmd.dat
+echo quote site filetype=seq>> ftpcmd.tmp
 
 rem --
-echo quit>> ftpcmd.dat
-ftp -n -s:ftpcmd.dat %HOSTNAME%
+echo quit>> ftpcmd.tmp
+ftp -n -s:ftpcmd.dat %ProjectMVShostname%
 rem del ftpcmd.dat
 rem type result1.txt
