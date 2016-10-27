2ba85ac9ed95e51fa12124b9deb19a0e25e999fd
kat rondina <katroserondina@gmail.com>
2016-10-28 01:48:59 +0800

    update compiler.bat with correct variables

--- compiler.bat
+++ compiler.bat
@@ -1,17 +1,17 @@
 @echo off
 
 rem -- enter your username and your password
-echo user %username%> ftpcmd.tmp
-echo %password%>> ftpcmd.tmp
+echo user %katrondinausername%> ftpcmd.tmp
+echo %katrondinapassword%>> ftpcmd.tmp
 
 rem -- put "input file" 'PDS/DS destination'
-rem echo put "%WORKSPACE%\DEVOPS.PPLI\HELLOWW.pli" 'b136901.devops.ppli(helloww)'>> ftpcmd.tmp
-rem echo put "%WORKSPACE%\DEVOPS.JCL\PLAHELLO.jcl" 'b136901.devops.jcl(plahello)'>> ftpcmd.tmp
+echo put "%WORKSPACE%\DEVOPS.PPLI\HELLO.pli" 'b136901.devops.ppli(hello)'>> ftpcmd.tmp
+echo put "%WORKSPACE%\DEVOPS.JCL\PLAHELLO.jcl" 'b136901.devops.jcl(plahello)'>> ftpcmd.tmp
 
 rem -- to connect to MVS JES Subcommand
 echo quote site filetype=jes>> ftpcmd.tmp
 rem -- ftp compile jcl to jes
-rem echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
+echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
 rem -- to view jobs in spool
 echo dir>> ftpcmd.tmp
 rem -- to go back to working directory
@@ -19,6 +19,6 @@ echo quote site filetype=seq>> ftpcmd.tmp
 
 rem --
 echo quit>> ftpcmd.tmp
-ftp -n -s:ftpcmd.tmp %hostname%
+ftp -n -s:ftpcmd.tmp %ProjectMVShostname%
 rem del ftpcmd.tmp
 rem type result1.txt
