9312e3e3958f8cfc684c964980bfe55d04818b7a
kat rondina <rondink@ph.ibm.com>
2016-11-10 19:58:03 +0800

    test1

--- compiler.bat
+++ compiler.bat
@@ -5,13 +5,13 @@ echo user %katrondinausername%> ftpcmd.tmp
 echo %katrondinapassword%>> ftpcmd.tmp
 
 rem -- put "input file" 'PDS/DS destination'
-echo put "%WORKSPACE%\DEVOPS.PPLI\HELLO.pli" 'b136901.devops.ppli(hello)'>> ftpcmd.tmp
+echo rem put "%WORKSPACE%\DEVOPS.PPLI\HELLO.pli" 'b136901.devops.ppli(hello)'>> ftpcmd.tmp
 echo rem put "%WORKSPACE%\DEVOPS.JCL\PLBHELLO.jcl" 'b136901.devops.jcl(plbhello)'>> ftpcmd.tmp
 
 rem -- to connect to MVS JES Subcommand
 echo quote site filetype=jes>> ftpcmd.tmp
 rem -- ftp compile jcl to jes
-echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
+echo rem get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
 rem -- to view jobs in spool
 echo dir>> ftpcmd.tmp
 rem -- to go back to working directory
