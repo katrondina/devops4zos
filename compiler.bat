e9ac577eed80cfc9831135a23a7c440edaf80419
kat rondina <rondink@ph.ibm.com>
2017-03-27 23:31:13 +0800

    testing jenkins only. no running of jobs.

--- compiler.bat
+++ compiler.bat
@@ -5,17 +5,17 @@ echo user %katrondinausername%> ftpcmd.tmp
 echo %katrondinapassword%>> ftpcmd.tmp
 
 REM ----------- PUT "INPUT FILE" 'PDS/DS DESTINATION'
-echo put "%WORKSPACE%\DEVOPS.PPLI\HELLO.pli" 'b136901.devops.ppli(hello)'>> ftpcmd.tmp
-echo put "%WORKSPACE%\DEVOPS.JCL\PLBHELLO.jcl" 'b136901.devops.jcl(plbhello)'>> ftpcmd.tmp
+rem echo put "%WORKSPACE%\DEVOPS.PPLI\HELLO.pli" 'b136901.devops.ppli(hello)'>> ftpcmd.tmp
+rem echo put "%WORKSPACE%\DEVOPS.JCL\PLBHELLO.jcl" 'b136901.devops.jcl(plbhello)'>> ftpcmd.tmp
 
 REM ----------- TO CONNECT TO MVS JES SUBCOMMAND
-echo quote site filetype=jes>> ftpcmd.tmp
+rem echo quote site filetype=jes>> ftpcmd.tmp
 REM ----------- FTP COMPILE JCL TO JES AND SAVE SPOOL TO RESULT1.TXT
-echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
+rem echo get 'b136901.devops.jcl(plahello)' result1.txt>>ftpcmd.tmp
 REM ----------- TO VIEW HELD JOBS IN SPOOL
-echo dir>> ftpcmd.tmp
+rem echo dir>> ftpcmd.tmp
 REM ----------- BACK TO WORKING DIRECTORY
-echo quote site filetype=seq>> ftpcmd.tmp
+rem echo quote site filetype=seq>> ftpcmd.tmp
 
 REM ---- DISCONNECT AND QUIT FTP
 echo disconnect>> ftpcmd.tmp
